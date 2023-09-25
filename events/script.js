7a527aed427b2d95548cbf28a4d7f61005bb29a9
tim <timdesmet00@gmail.com>
2023-09-25 08:03:55 +0200

    verif pas sur d'avoir push vendredi soir

--- events/script.js
+++ events/script.js
@@ -7,7 +7,13 @@ const getElapsedTime = () => {
 const clickOnSquare = (e) => {
   console.log(e.target.classList[1]);
   console.log(getElapsedTime());
-  greenSquare(); //ajout de la fonction a l'event
+  if (e.target.classList[1] === "green") {
+    greenSquare();
+  } else if (e.target.classList[1] === "violet") {
+    violetSquare();
+  } else if (e.target.classList[1] === "orange") {
+    orangeSquare();
+  }
 };
 
 const actionSquares = document.querySelectorAll(".actionsquare");
@@ -20,16 +26,76 @@ for (let actionSquare of actionSquares) {
 const squareGreen = document.getElementsByClassName("green");
 const squareViolet = document.getElementsByClassName("violet");
 const squareOrange = document.getElementsByClassName("orange");
-const displayedSquare = document.getElementsByClassName(
-  "displayedsquare-wrapper"
-);
+const displayedSquare = document.querySelector(".displayedsquare-wrapper");
 const actionLog = document.querySelector("ul");
 
-let greenSquare = () => {
-  let div = displayedSquare.createElement("div");
-  let Text = displayedSquare.createTextNode(e.target.classList[1]);
-  div.classList.add("displayedSquareGreen");
+const greenSquare = () => {
+  let divGreen = document.createElement("div");
+  divGreen.classList.add("actionsquare", "green", "displayedsquare");
 
-  actionLog.createElement("li");
-  actionLog.createTextNode(Number + " , created a new square green");
+  let timeLog = document.createElement("li");
+  timeLog.textContent = `${getElapsedTime()}, created a new green square`;
+
+  displayedSquare.appendChild(divGreen).addEventListener("click", () => {
+    alert("Green");
+  });
+  actionLog.appendChild(timeLog);
 };
+
+const violetSquare = () => {
+  let divViolet = document.createElement("div");
+  divViolet.classList.add("actionsquare", "violet", "displayedsquare");
+
+  let timeLog = document.createElement("li");
+  timeLog.textContent = `${getElapsedTime()}, created a new violet square`;
+
+  displayedSquare.appendChild(divViolet).addEventListener("click", () => {
+    alert("Violet");
+  });
+  actionLog.appendChild(timeLog);
+};
+
+const orangeSquare = () => {
+  let divOrange = document.createElement("div");
+  divOrange.classList.add("actionsquare", "orange", "displayedsquare");
+
+  let timeLog = document.createElement("li");
+  timeLog.textContent = `${getElapsedTime()}, created a new orange square`;
+
+  displayedSquare.appendChild(divOrange).addEventListener("click", () => {
+    alert("Orange");
+  });
+  actionLog.appendChild(timeLog);
+};
+
+const keypress = document.querySelector("body");
+
+let randomColor = () => {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  return `rgb(${red}, ${green}, ${blue}`;
+};
+
+document.addEventListener("keypress", (e) => {
+  console.log(e.keyCode);
+  if (e.keyCode == "32") {
+    keypress.style.backgroundColor = randomColor();
+
+    let timeLog = document.createElement("li");
+    timeLog.textContent = `${getElapsedTime()}, you press space bar`;
+    actionLog.appendChild(timeLog);
+  } else if (e.keyCode == 108) {
+    actionLog.removeChild(actionLog.firstChild);
+  } else if (e.keyCode == 115) {
+    displayedSquare.removeChild(displayedSquare.firstChild);
+  }
+});
+
+let alertSquare = document.querySelector(".displayedsquare");
+
+alertSquare.addEventListener("click", () => {
+  console.log("lala");
+  alert("test");
+});
